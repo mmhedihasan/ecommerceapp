1608ff472b8d0a35bd11bcdeebaaccb527ec13a7
mmehedihasan <mmehedihasansagar@gmail.com>
2024-03-09 12:13:36 +0600

    OTP Verification UI Design Complete

--- lib/presentation/ui/screens/auth/otp_verification_screen.dart
+++ lib/presentation/ui/screens/auth/otp_verification_screen.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerceapp/presentation/ui/utility/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 import '../../utility/image_assets.dart';
 
@@ -15,36 +17,110 @@ class _OTPVerificationScreenState extends State<OTPVerificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 80,),
-              Center(
-                  child: SvgPicture.asset(
-                    ImagesAssets.craftyBayLogoSVG,
-                    width: 100,
-                  )),
-              const SizedBox(height: 16,),
-              Text("Enter Your OTP Code", style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                fontSize: 28,
-              ),),
-              const SizedBox(height: 5,),
-              Text("A 4 digit code has been sent", style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                color: Colors.grey,
-                fontSize: 16,
-              ),),
-              const SizedBox(height: 22,),
-              TextFormField(),
-              const SizedBox(height: 16,),
-              SizedBox(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Center(
+                    child: SvgPicture.asset(
+                  ImagesAssets.craftyBayLogoSVG,
+                  width: 100,
+                )),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Enter Your OTP Code",
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        fontSize: 28,
+                      ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "A 4 digit code has been sent",
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                ),
+                const SizedBox(
+                  height: 22,
+                ),
+                PinCodeTextField(
+                  length: 4,
+                  obscureText: false,
+                  animationType: AnimationType.fade,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  keyboardType: TextInputType.number,
+                  pinTheme: PinTheme(
+                    shape: PinCodeFieldShape.box,
+                    borderRadius: BorderRadius.circular(5),
+                    fieldHeight: 50,
+                    fieldWidth: 50,
+                    activeFillColor: Colors.white,
+                    inactiveFillColor: Colors.white,
+                    selectedFillColor: Colors.white,
+                    activeColor: AppColors.primarySwatch,
+                    inactiveColor: AppColors.primarySwatch,
+                    selectedColor: Colors.green,
+                  ),
+                  animationDuration: Duration(milliseconds: 300),
+                  enableActiveFill: true,
+                  onCompleted: (v) {
+          
+                  },
+                  onChanged: (value) {
+          
+                  },
+                  beforeTextPaste: (text) {
+                    return true;
+                  }, appContext: context,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                      onPressed: (){
-
-                      }, child: const Text("Next")))
-
-            ],
+                    onPressed: () {},
+                    child: const Text("Next"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                RichText(
+                  text: const TextSpan(
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    children: [
+                      TextSpan(text: "This code expire in "),
+                      TextSpan(
+                        text: "120s",
+                        style: TextStyle(
+                          color: AppColors.primarySwatch,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.grey,
+                  ),
+                  child: const Text("Resend"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
