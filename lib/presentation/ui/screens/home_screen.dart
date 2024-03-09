1fe6b907aa1dea6d24edd4e39165c0161ca5296d
mmehedihasan <mmehedihasansagar@gmail.com>
2024-03-09 22:22:27 +0600

    Home Screen Appbar Design Work Running

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,11 +1,70 @@
+import 'package:ecommerceapp/presentation/ui/utility/image_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Row(
+            children: [
+              SvgPicture.asset(
+                ImagesAssets.craftyBayNavLogoSVG,
+              ),
+              const Spacer(),
+              CircularIconButton(
+                icon: Icons.person,
+                onTap: () {},
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              CircularIconButton(
+                icon: Icons.call,
+                onTap: () {},
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              CircularIconButton(
+                icon: Icons.notifications_none,
+                onTap: () {},
+              ),
+            ],
+          )),
+    );
   }
 }
 
+class CircularIconButton extends StatelessWidget {
+  const CircularIconButton({
+    super.key,
+    required this.icon,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(30),
+      child: CircleAvatar(
+        radius: 15,
+        backgroundColor: Colors.grey.shade300,
+        child: Icon(
+          icon,
+          color: Colors.grey,
+          size: 18,
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/ui/screens/splash_screen.dart
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerceapp/presentation/ui/screens/auth/email_verification_screen.dart';
+import 'package:ecommerceapp/presentation/ui/screens/home_screen.dart';
 import 'package:ecommerceapp/presentation/ui/utility/image_assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -22,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
   void goToNextScreen(){
     Future.delayed(const Duration(seconds: 1)).then((value) {
-     Get.offAll(const EmailVerificationScreen());
+     Get.offAll(const HomeScreen());
     });
   }
 
