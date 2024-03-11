7721592d8bb530a3c47f30de87c5c5b83e1ef827
mmehedihasan <mmehedihasansagar@gmail.com>
2024-03-11 15:27:49 +0600

    Home Page Search bar and carosol uo design

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,68 +1,122 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerceapp/presentation/ui/utility/app_colors.dart';
 import 'package:ecommerceapp/presentation/ui/utility/image_assets.dart';
+import 'package:ecommerceapp/presentation/ui/widgets/circular_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ValueNotifier<int> _selectedSlider= ValueNotifier(0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: Row(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+            SvgPicture.asset(
+              ImagesAssets.craftyBayNavLogoSVG,
+            ),
+            const Spacer(),
+            CircularIconButton(
+              icon: Icons.person,
+              onTap: () {},
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            CircularIconButton(
+              icon: Icons.call,
+              onTap: () {},
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            CircularIconButton(
+              icon: Icons.notifications_none,
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
             children: [
-              SvgPicture.asset(
-                ImagesAssets.craftyBayNavLogoSVG,
-              ),
-              const Spacer(),
-              CircularIconButton(
-                icon: Icons.person,
-                onTap: () {},
+              TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: "Search",
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                ),
               ),
               const SizedBox(
-                width: 8,
+                height: 16,
               ),
-              CircularIconButton(
-                icon: Icons.call,
-                onTap: () {},
+              CarouselSlider(
+                options: CarouselOptions(
+                    height: 180.0,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 4),
+                    onPageChanged: (int page, _) {
+                      _selectedSlider.value = page;
+                    },),
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: const BoxDecoration(color: Colors.amber),
+                          child: Text(
+                            'text $i',
+                            style: const TextStyle(fontSize: 16.0),
+                          ),);
+                    },
+                  );
+                }).toList(),
               ),
-              const SizedBox(
-                width: 8,
-              ),
-              CircularIconButton(
-                icon: Icons.notifications_none,
-                onTap: () {},
-              ),
+              const SizedBox(height: 16,),
+              ValueListenableBuilder(valueListenable: _selectedSlider, builder: (context, value, _) {
+                List<Widget>list = [];
+                for(int i=0; i<5; i++){
+                 list.add(Container(
+                    height: 15,
+                    width: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(18),
+                      color: value == i ? AppColors.primarySwatch : null
+                    ),
+                  ),);
+                }
+                return Row(
+                  children: list,
+                );
+              },),
+
             ],
-          )),
-    );
-  }
-}
-
-class CircularIconButton extends StatelessWidget {
-  const CircularIconButton({
-    super.key,
-    required this.icon,
-    required this.onTap,
-  });
-
-  final IconData icon;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(30),
-      child: CircleAvatar(
-        radius: 15,
-        backgroundColor: Colors.grey.shade200,
-        child: Icon(
-          icon,
-          color: Colors.grey,
-          size: 17,
+          ),
         ),
       ),
     );
