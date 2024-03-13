f115b72ee1ff26a7cc478f5a207121c8a9067355
mmehedihasan <mmehedihasansagar@gmail.com>
2024-03-13 16:07:46 +0600

    Popular new special product card desgin complete

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:ecommerceapp/presentation/ui/widgets/category_card.dart';
 import 'package:ecommerceapp/presentation/ui/widgets/circular_icon_button.dart';
 import 'package:ecommerceapp/presentation/ui/widgets/home/home_slider.dart';
 import 'package:ecommerceapp/presentation/ui/widgets/home/section_header.dart';
+import 'package:ecommerceapp/presentation/ui/widgets/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -102,12 +103,41 @@ class _HomeScreenState extends State<HomeScreen> {
                 onTap: () {},
               ),
               SizedBox(
-                height: 180,
+                height: 175,
                 child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: 20,
                     itemBuilder: (context, index){
-
+                  return const ProductCard();
+                } ),
+              ), const SizedBox(
+                height: 16,
+              ),
+              SectionHeader(
+                title: 'Special',
+                onTap: () {},
+              ),
+              SizedBox(
+                height: 175,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 20,
+                    itemBuilder: (context, index){
+                  return const ProductCard();
+                } ),
+              ), const SizedBox(
+                height: 16,
+              ),
+              SectionHeader(
+                title: 'New',
+                onTap: () {},
+              ),
+              SizedBox(
+                height: 175,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 20,
+                    itemBuilder: (context, index){
                   return const ProductCard();
                 } ),
               ),
@@ -120,94 +150,4 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class ProductCard extends StatelessWidget {
-  const ProductCard({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shadowColor: AppColors.primarySwatch.withOpacity(0.1),
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: SizedBox(
-        width: 130,
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-              decoration: BoxDecoration(
-                color: AppColors.primarySwatch.withOpacity(0.1),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(8),
-                  topRight: Radius.circular(8),
-                ),
-                image: const DecorationImage(
-                 image: AssetImage(
-                   ImagesAssets.craftyshoe,
-                ),fit: BoxFit.contain),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Nike shoe a501205",
-                    maxLines: 1,
-                    style: TextStyle(
-                      overflow: TextOverflow.ellipsis,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.blueGrey,
-                    ),
-                  ),
-                  SizedBox(height: 2,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("\$90", style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: AppColors.primarySwatch,
-                      ),),
-                      Wrap(
-                         crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.star,
-                            size: 18,
-                            color: Colors.amber,
-                          ),
-                          Text("4.5", style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.blueGrey,
-                          ),),
-                        ],
-                      ),
-                      Card(
-                        color: AppColors.primarySwatch,
-                        child: Padding(
-                          padding: EdgeInsets.all(3.0),
-                          child: Icon(
-                            Icons.favorite_border,
-                            size: 12,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
