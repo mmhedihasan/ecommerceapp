223f749ccf347b18e8533ed06a0e40b215324dd6
mmehedihasan <mmehedihasansagar@gmail.com>
2024-03-19 17:47:12 +0600

    Product Dertails Screen Colors selected design

--- lib/presentation/ui/screens/product_details_screen.dart
+++ lib/presentation/ui/screens/product_details_screen.dart
@@ -1,43 +1,165 @@
+import 'package:ecommerceapp/presentation/ui/utility/app_colors.dart';
 import 'package:ecommerceapp/presentation/ui/widgets/custom_stepper.dart';
 import 'package:ecommerceapp/presentation/ui/widgets/home/product_image_slider.dart';
 import 'package:flutter/material.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   const ProductDetailsScreen({super.key});
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  List<Color> colors = [
+    Colors.deepOrange,
+    Colors.amber,
+    Colors.blue,
+    Colors.yellow,
+    Colors.black,
+    Colors.pink,
+  ];
+  int _selectedColorIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  SafeArea(
+      body: SafeArea(
         child: Column(
           children: [
             Stack(
               children: [
-                 ProductImageSlider(),
+                const ProductImageSlider(),
                 AppBar(
                   leading: const BackButton(
                     color: Colors.black54,
                   ),
-                  title: Text("Product Details", style: TextStyle(
-                    color: Colors.black54,
-                  ),),
+                  title: const Text(
+                    "Product Details",
+                    style: TextStyle(
+                      color: Colors.black54,
+                    ),
+                  ),
                   backgroundColor: Colors.transparent,
                   elevation: 0,
                 ),
               ],
             ),
-            Row(
-              children: [
-                const Expanded(child: Text("Adder Shoe Black4050, Black Edition", style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 0.5,
-                ),),),
-                CustomStepper(lowerLimit: 1, upperLimit: 10, stepValue: 1, value: 1, onChange: (newValue){
-                    print(newValue);
-                },),
-              ],
-            )
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          "Adder Shoe Black4050, Black Edition",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                      CustomStepper(
+                        lowerLimit: 1,
+                        upperLimit: 10,
+                        stepValue: 1,
+                        value: 1,
+                        onChange: (newValue) {
+                          print(newValue);
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.star,
+                            size: 18,
+                            color: Colors.amber,
+                          ),
+                          Text(
+                            "4.5",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Review",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.primarySwatch,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Card(
+                        color: AppColors.primarySwatch,
+                        child: Padding(
+                          padding: EdgeInsets.all(3.0),
+                          child: Icon(
+                            Icons.favorite_border,
+                            size: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Color",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 25,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: colors.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          borderRadius: BorderRadius.circular(20),
+                          onTap: () {
+                            _selectedColorIndex = index;
+                            if (mounted) {
+                              setState(() {});
+                            }
+                          },
+                          child: CircleAvatar(
+                            backgroundColor: colors[index],
+                            child: _selectedColorIndex == index
+                                ? Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : null,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
